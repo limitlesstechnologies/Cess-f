530f3afb6835a5faf98ed307db34fe58322cd992
limitlesstechnologies <155821618+limitlesstechnologies@users.noreply.github.com>
2024-01-06 06:49:23 -0800

    Add files via upload

---
+++ cess-foundation-main project/Cess Foundation/send.php
@@ -0,0 +1,8 @@
+<?php
+$to = "0791044683@vtext.com" ;
+$from = "cessfoundation.netlify.app";
+$message = "Thankyou for vsiting us send your donations to 
+0721857782  (cecilia)"
+$headers = "From: $from\n";
+mail ($to, '',$message, $headers)
+?>
